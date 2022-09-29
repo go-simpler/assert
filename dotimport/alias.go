807f33e9b2702c1e054c73419891f46ac80b68ca
junk1tm <github@junk1tm.me>
2022-09-29 21:32:24 +0400

    docs: add dotimport example

--- dotimport/alias.go
+++ dotimport/alias.go
@@ -1,6 +1,19 @@
 // Package dotimport provides type aliases for the parent [assert] package. It
 // is intended to be imported using dot syntax so that [E] and [F] can be used
 // as if they were local types.
+//
+//	package foo_test
+//
+//	import (
+//		"testing"
+//
+//		"assert"
+//		. "assert/dotimport"
+//	)
+//
+//	func TestFoo(t *testing.T) {
+//		assert.NoErr[E](t, foo.Foo())
+//	}
 package dotimport
 
 import "github.com/junk1tm/assert"
